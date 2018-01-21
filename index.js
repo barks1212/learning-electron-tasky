e6aa339aa12a514bfa392fcb3ba57a896f3cce63
barks1212 <willbarkerwork@gmail.com>
2018-01-21 17:29:31 +0000

    specced main window & added OS diverse tray icons

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const path = require('path');
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false, //removes status bar at top of app
+    resizable: false, //does not allow user to resize app window
+  });
   mainWindow.loadURL(`file://${__dirname}/src/index.html`)
-})
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  new Tray(iconPath)
+});
 
